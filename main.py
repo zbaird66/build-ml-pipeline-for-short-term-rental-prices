19b114ca224f950f9da2221e38446c8f512e7233
zach <zbaird66@gmail.com>
2024-04-25 15:39:25 -0400

    Production-v1.0.3

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def go(config: DictConfig):
                 parameters={
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
-                    "kl_threshold": config['data_check']['k1_threshold'],
+                    "kl_threshold": config['data_check']['kl_threshold'],
                     "min_price": config['data_check']['min_price'],
                     "max_price": config['data_check']['max_price']
                 },
